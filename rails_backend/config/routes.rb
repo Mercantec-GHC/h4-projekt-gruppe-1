eac3e8a81f4a792b4fba6c708ea19f493d70e43d
Kim Mortensen <61045621+krixzy@users.noreply.github.com>
2024-08-13 15:07:25 +0200

    adding user again

--- rails_backend/config/routes.rb
+++ rails_backend/config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 
   # get '/user/:name', to: 'user#show', as: 'user_by_name'
   resources :user_stat
-  # resources :user
+  resources :user
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
