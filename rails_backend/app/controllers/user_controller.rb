6ffdbe74a9e4f9744c08fa57cfb60547337a61d2
Kim Mortensen <61045621+krixzy@users.noreply.github.com>
2024-08-13 14:49:20 +0200

    adding user_controller again

--- rails_backend/app/controllers/user_controller.rb
+++ rails_backend/app/controllers/user_controller.rb
@@ -1,48 +1,48 @@
-# class UserController < ApplicationController
-#   def index
-#     user = User.all
-#     render json: user , except: [:id, :created_at, :updated_at], status: :ok
-#   end
+class UserController < ApplicationController
+  def index
+    user = User.all
+    render json: user , except: [:id, :created_at, :updated_at], status: :ok
+  end
 
-#   def show
-#     user = User.find_by(name: params[:name])
-#     render json: user, except: [:id, :created_at, :updated_at], status: :ok
-#   end
+  def show
+    user = User.find_by(name: params[:name])
+    render json: user, except: [:id, :created_at, :updated_at], status: :ok
+  end
 
-#   def create
-#     user = User.new(user_params)
-#     user.user_stat = UserStat.create(wins: 0, lost: 0, games_played: 0, right_guesses: 0, skips: 0)
-#     if user.save
-#       render json: user, status: :created
-#     else
-#       render json: user.errors, status: :unprocessable_entity
-#     end
+  def create
+    user = User.new(user_params)
+    user.user_stat = UserStat.create(wins: 0, lost: 0, games_played: 0, right_guesses: 0, skips: 0)
+    if user.save
+      render json: user, status: :created
+    else
+      render json: user.errors, status: :unprocessable_entity
+    end
   
-#   end
+  end
 
-#   def update
-#     user = User.find(params[:id])
-#     if user.nil?
-#       render jsom: {error: "User not found"}, status: :not_found 
-#       return
-#     end
-#     if user.update(user_params)
-#       render json: user, status: :ok
-#     else
-#       render json: user.errors, status: :unprocessable_entity
-#     end
-#   end
+  def update
+    user = User.find(params[:id])
+    if user.nil?
+      render jsom: {error: "User not found"}, status: :not_found 
+      return
+    end
+    if user.update(user_params)
+      render json: user, status: :ok
+    else
+      render json: user.errors, status: :unprocessable_entity
+    end
+  end
 
-#   def destroy
-#     user = User.find(params[:id])
-#     user_stat = UserStat.find(user.user_stat.id)
-#     user.destroy
-#     user_stat.destroy
-#     render json: user, status: :ok
-#   end
-# end
+  def destroy
+    user = User.find(params[:id])
+    user_stat = UserStat.find(user.user_stat.id)
+    user.destroy
+    user_stat.destroy
+    render json: user, status: :ok
+  end
+end
 
-# private
-# def user_params
-#   reparams.require(:user).permit(:name, :nick_name, :email, :password, :number)
-# end
+private
+def user_params
+  reparams.require(:user).permit(:name, :nick_name, :email, :password, :number)
+end
