a523d6fb6a2ba2339325b8fc78c1f8a2363a268b
DMSteenberg <dsj@digitalrevisor.nu>
2024-08-22 14:32:33 +0200

    sorry kim

--- guess_that_beatboxer/lib/pages/lobby.dart
+++ guess_that_beatboxer/lib/pages/lobby.dart
@@ -2,13 +2,3 @@ import 'package:flutter/material.dart';
 import 'package:guess_that_beatboxer/widgets/appBar.dart';  
 import 'package:guess_that_beatboxer/Widgets/appBar.dart';
 
-class Lobby extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: appBarFunction("Lobby"),
-      body: Text("test"),
-    );
-  }
-  
-}
