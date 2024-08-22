9ea2f42fbb2a1d6d0658d743669b050ec65b95f3
Kim Mortensen <61045621+krixzy@users.noreply.github.com>
2024-08-22 14:37:04 +0200

    fixed lobby

--- guess_that_beatboxer/lib/pages/lobby.dart
+++ guess_that_beatboxer/lib/pages/lobby.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:guess_that_beatboxer/widgets/appBar.dart';  
 import 'package:guess_that_beatboxer/Widgets/appBar.dart';
 
 class Lobby extends StatelessWidget {
