b7daea9f045bf5fb0c9d3931c3eabd6db46a07e2
DMSteenberg <dsj@digitalrevisor.nu>
2024-08-22 09:43:20 +0200

    fejl 40

--- JWT/routes/login.go
+++ JWT/routes/login.go
@@ -46,7 +46,7 @@ func Login(c *gin.Context) {
 		return
 	}
 
-	token, err := util.CreateToken(user.Name, user.Email, user.Phone, user.NickName)
+	token, err := util.CreateToken(user.Name, user.Email, user.Phone, user.NickName, user.ID)
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to create token"})
 		return
