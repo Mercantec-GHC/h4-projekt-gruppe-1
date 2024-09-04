a5279d04d947e8ad5624916466e552ab0c2a6503
Kim Mortensen <61045621+krixzy@users.noreply.github.com>
2024-09-04 14:04:25 +0200

    more fixes

--- guess_that_beatboxer/macos/Flutter/GeneratedPluginRegistrant.swift
+++ guess_that_beatboxer/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,6 +6,8 @@ import FlutterMacOS
 import Foundation
 
 import file_selector_macos
+import firebase_core
+import firebase_messaging
 import flutter_local_notifications
 import flutter_secure_storage_macos
 import path_provider_foundation
