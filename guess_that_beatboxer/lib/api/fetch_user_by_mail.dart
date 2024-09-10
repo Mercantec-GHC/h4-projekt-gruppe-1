4e2bda50b1666f5f5ae8be651d78b4bda15b62a2
DMSteenberg <dsj@digitalrevisor.nu>
2024-09-10 18:04:26 +0200

    hvis man ikke glemmer noget er det kedeligt

--- guess_that_beatboxer/lib/api/fetch_user_by_mail.dart
+++ guess_that_beatboxer/lib/api/fetch_user_by_mail.dart
@@ -3,8 +3,8 @@ import 'package:http/http.dart' as http;
 
 Future<bool> fetchByMail(String email) async {
   final response = await http.get(
-    /* Uri.parse('https://h4-projekt-gruppe-1.onrender.com/user/email/$email'), */
-    Uri.parse('http://localhost:8080/user/email/$email'),
+    Uri.parse('https://h4-projekt-gruppe-1.onrender.com/user/email/$email'),
+   /*  Uri.parse('http://localhost:8080/user/email/$email'), */
     headers: <String, String>{
       'Content-Type': 'application/json; charset=UTF-8',
     },
