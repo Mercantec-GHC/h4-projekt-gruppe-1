11d986c31964feb66d63a77263ee526b5a06be30
KevinNielsen00 <122385622+KevinNielsen00@users.noreply.github.com>
2024-09-10 15:16:41 +0200

    Update recent games
    Taget i brug så den bruger Daniels popup

--- guess_that_beatboxer/lib/Widgets/playerFeedback.dart
+++ guess_that_beatboxer/lib/Widgets/playerFeedback.dart
@@ -2,142 +2,19 @@ import 'package:flutter/material.dart';
 import 'package:guess_that_beatboxer/main.dart';
 import 'package:provider/provider.dart';
 import '../models/user.dart';
-import '../models/match_history.dart'; 
+import '../models/match_history.dart';
+import '../Widgets/userStats/showmatch.dart';
 
 class MatchTile extends StatelessWidget {
   final MatchHistory match;
 
   MatchTile({required this.match});
 
-  void showMatchDetailsDialog(BuildContext context) {
-    final formattedDate = "${match.createdAt.day}/${match.createdAt.month}/${match.createdAt.year}";
-
-    showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          title: const Text(
-            'Match Details',
-            style: TextStyle(
-              fontSize: 24,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-          content: Container(
-            width: MediaQuery.of(context).size.width * 0.8,
-            child: SingleChildScrollView(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Text(
-                    'Winner: ${match.winner}',
-                    style: const TextStyle(
-                      color: Colors.green,
-                      fontSize: 18,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  const SizedBox(height: 8),
-                  Text(
-                    'Loser: ${match.loser}',
-                    style: const TextStyle(
-                      color: Colors.red,
-                      fontSize: 18,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  const SizedBox(height: 16),
-                  Text(
-                    '${match.player_1_user_name} match info',
-                    style: const TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  const Divider(),
-                  Text(
-                    'Username: ${match.player_1_user_name}',
-                    style: const TextStyle(fontSize: 16),
-                  ),
-                  const SizedBox(height: 8),
-                  Text(
-                    'Points: ${match.player_1_points}',
-                    style: const TextStyle(fontSize: 16),
-                  ),
-                  const SizedBox(height: 8),
-                  Text(
-                    'Comment: ${match.player_1_comment}',
-                    style: const TextStyle(fontSize: 16),
-                  ),
-                  const SizedBox(height: 16),
-                  Text(
-                    '${match.player_2_user_name} match info',
-                    style: const TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  const Divider(),
-                  Text(
-                    'Username: ${match.player_2_user_name}',
-                    style: const TextStyle(fontSize: 16),
-                  ),
-                  const SizedBox(height: 8),
-                  Text(
-                    'Points: ${match.player_2_points}',
-                    style: const TextStyle(fontSize: 16),
-                  ),
-                  const SizedBox(height: 8),
-                  Text(
-                    'Comment: ${match.player_2_comment}',
-                    style: const TextStyle(fontSize: 16),
-                  ),
-                  const SizedBox(height: 16),
-                  const Text(
-                    'Match Details',
-                    style: TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  const Divider(),
-                  Text(
-                    'Draw: ${match.draw ? "Yes" : "No"}',
-                    style: const TextStyle(fontSize: 16),
-                  ),
-                  const SizedBox(height: 8),
-                  Text(
-                    'Played At: $formattedDate',
-                    style: const TextStyle(fontSize: 16),
-                  ),
-                ],
-              ),
-            ),
-          ),
-          actions: <Widget>[
-            TextButton(
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-              child: const Text(
-                'Close',
-                style: TextStyle(
-                  fontSize: 16,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-            ),
-          ],
-        );
-      },
-    );
-  }
-
   @override
   Widget build(BuildContext context) {
     var user = context.read<MyAppState>().user;
     return GestureDetector(
-      onTap: () => showMatchDetailsDialog(context),
+      onTap: () => showMatch(context, match),
       child: Card(
         child: Padding(
           padding: const EdgeInsets.all(8.0),
